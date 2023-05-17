5e093d58e49cdce02ee3e3a4003d16753a7b32fb
Jesse Osborne <jesseosborne615@gmail.com>
2023-05-17 19:48:27 -0400

    added base files

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,10 @@
+import './index.scss'
+import me from '../../assets/images/me.jpeg'
+
+const Sidebar = () => (
+    <div className='nav-bar'>
+    <img src= {me} alt ="Jesse" />
+    </div>
+)
+
+export default Sidebar
