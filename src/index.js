857b742bc8782975a42aeaf5f113da6bec2ba0c8
Jesse Osborne <jesseosborne615@gmail.com>
2023-05-18 15:29:24 -0400

    added basename in BrowserRouter in index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,8 @@ import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/portfolio'>
+   
     <App />
     </BrowserRouter>
   </React.StrictMode>
